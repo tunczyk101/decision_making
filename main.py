d956c28846916ddaf6065d53bd722fb15cf0c83e
Tobiasz Szulc <szulc.tobiasz@gmail.com>
2022-12-11 19:19:37 +0100

    merge fix

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from matrix.AHP import AHP
+from back.AHP import AHP
 
-# criteria = ["lokalizacja", "obsluga", "wystroj", "ilosc smakow", "jakosc lodow", "cena", "jakosc sorbetow"]
+criteria = ["lokalizacja", "obsluga", "wystroj", "ilosc smakow", "jakosc lodow", "cena", "jakosc sorbetow"]
 propositions = ["Good Lood Miasteczko AGH", "Wadowice", "Tiffany Ice Cream"]
-criteria = ["jakosc lodow", "cena", "jakosc sorbetow"]
+# criteria = ["jakosc lodow", "cena", "jakosc sorbetow"]
 
 if __name__ == "__main__":
     ahp = AHP(criteria, propositions)
-    ahp.ask_criteria_questions()
+    ahp.start()
+
 
